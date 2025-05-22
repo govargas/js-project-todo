c05499fa51557b0782e6430556d820247e273ca6
Talo Vargas <govargas@gmail.com>
2025-05-22 23:11:05 +0200

    sketch UI and breakt it into components

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function TodoItem() {
+  return (
+    <header className="p-4">
+      {/* TODO: App title + ThemeToggle */}
+      <h1 className="text-2xl font-bold">Todo App</h1>
+    </header>
+  );
+}
