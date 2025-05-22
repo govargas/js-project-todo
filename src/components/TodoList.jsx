b50e6fa08ba0298e235c12edf41b55a4aae2a587
Talo Vargas <govargas@gmail.com>
2025-05-22 23:50:34 +0200

    global state wired up

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,27 +1,31 @@
+// src/components/TodoList.jsx
 import React from 'react'
 import { useTodos } from '../store/useTodos'
 
 export default function TodoList() {
-  const todos = useTodos((state) => state.todos)
+  const todos      = useTodos((s) => s.todos)
+  const toggleTodo = useTodos((s) => s.toggleTodo)
+  const removeTodo = useTodos((s) => s.removeTodo)
 
   return (
     <ul className="space-y-2">
       {todos.map((todo) => (
         <li
           key={todo.id}
-          className={`flex items-center justify-between p-2 border rounded
-            ${todo.done ? 'opacity-50 line-through' : ''}`}
+          className={`flex items-center justify-between p-2 border rounded ${
+            todo.done ? 'opacity-50 line-through' : ''
+          }`}
         >
           <span>{todo.text}</span>
           <div className="space-x-2">
             <button
-              onClick={() => useTodos.getState().toggleTodo(todo.id)}
+              onClick={() => toggleTodo(todo.id)}
               className="px-2 py-1 border rounded"
             >
               {todo.done ? 'Undo' : 'Done'}
             </button>
             <button
-              onClick={() => useTodos.getState().removeTodo(todo.id)}
+              onClick={() => removeTodo(todo.id)}
               className="px-2 py-1 border rounded text-red-600"
             >
               ✕
