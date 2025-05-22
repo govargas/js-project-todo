df9f203244bdf602dbe72182e94718dce2d53d37
Talo Vargas <govargas@gmail.com>
2025-05-22 23:43:23 +0200

    hook up TodoList to the store

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React from 'react'
+import { useTodos } from '../store/useTodos'
 
 export default function TodoList() {
+  const todos = useTodos((state) => state.todos)
+
   return (
-    <header className="p-4">
-      {/* TODO: App title + ThemeToggle */}
-      <h1 className="text-2xl font-bold">Todo List</h1>
-    </header>
-  );
+    <ul className="space-y-2">
+      {todos.map((todo) => (
+        <li
+          key={todo.id}
+          className={`flex items-center justify-between p-2 border rounded
+            ${todo.done ? 'opacity-50 line-through' : ''}`}
+        >
+          <span>{todo.text}</span>
+          <div className="space-x-2">
+            <button
+              onClick={() => useTodos.getState().toggleTodo(todo.id)}
+              className="px-2 py-1 border rounded"
+            >
+              {todo.done ? 'Undo' : 'Done'}
+            </button>
+            <button
+              onClick={() => useTodos.getState().removeTodo(todo.id)}
+              className="px-2 py-1 border rounded text-red-600"
+            >
+              ✕
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
 }
