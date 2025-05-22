b989bd35034d3e9503233cf04ce165aa0007e9e0
Talo Vargas <govargas@gmail.com>
2025-05-23 00:33:37 +0200

    edit Footer.jsx to render Stats component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
+import Stats from './Stats'
 
 export default function Footer() {
   return (
-    <header className="p-4">
-      {/* TODO: App title + ThemeToggle */}
-      <h1 className="text-2xl font-bold">Footer</h1>
-    </header>
-  );
+    <footer className="max-w-xl mx-auto">
+      <Stats />
+    </footer>
+  )
 }
